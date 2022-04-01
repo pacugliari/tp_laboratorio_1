69fe81ee5131a2350cea19bc78b1c5c76ab77096
PACugliari <pacugliari@hotmail.com>
2022-04-01 09:14:23 -0300

    Creacion de repositorio

---
+++ TP_[1]/Sources/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: 1 abr. 2022
+ *      Author: PACugliari
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	printf("Hola Mundo \n");
+
+	return 0;
+}
